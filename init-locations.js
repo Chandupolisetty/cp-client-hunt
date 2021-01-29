c5f5082a8ad2bc27d23cfd473aedb34e6795c111
chandu polisetty <chandrakanthpolisetty@gmail.com>
2021-01-29 10:20:24 -0600

    updated

--- init-locations.js
+++ init-locations.js
@@ -1,13 +1,12 @@
 export default [
-    {
-     // Name: '1121 N college dr',
-      //Latitude: 40.355892797957075, 
-      //Longitude: -94.88178928487832
-    //},
-    //{
-      Name: '1125 N college dr',
-      Latitude: 40.35611322788756, 
-      Longitude: -94.88192402040133  
-    }
+  {
+    Name: '1121 N college dr',
+    Latitude: 40.355892797957075,
+    Longitude: -94.88178928487832
+  },
+  {
+    Name: '1125 N college dr',
+    Latitude: 40.35611322788756,
+    Longitude: -94.88192402040133
+  }
 ];
-      
